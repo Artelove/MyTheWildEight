4bda2326502b7fefd809f8b19907befa19500710
Artelove <arteevil.off@gmail.com>
2022-08-23 10:32:50 +0300

    feat(Outline objects[Plugin]): new mechanic which draw outline at the object
    Drawing is done by adding new materials and mixing shaders at the object
    Can be setting Outline Color, Mode, Width
    
    add: Some test scenes, object, settings not wagging on the project

---
+++ Surforwat/Assets/Scripts/Plugins/QuickOutline/Scripts/OutlineConfig.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Outline", menuName = "Configs/Outline config", order = 1)]
+public class OutlineConfig :ScriptableObject
+{
+    [SerializeField] private Color _outlineColor;
+    [SerializeField] private Outline.Mode _outlineMode;
+    [SerializeField] private float _outlineWidth = 2f;
+
+    public Color OutlineColor => _outlineColor;
+    public Outline.Mode OutlineMode => _outlineMode;
+    public float OutlineWidth => _outlineWidth;
+}
